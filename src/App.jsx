2bd155470294434a74ad39ec633329768512657e
SidharthSoumya <sidharthsoumya@gmail.com>
2024-03-03 17:54:39 +0530

    Function component is converted to class component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import { nanoid } from 'nanoid'
 import './App.css'
 import TodoList from './components/TodoList'
-import { useState } from 'react'
+import { Component } from 'react'
 
-function App() {
-	const [todoList, setToDoList] = useState([])
-	const [listDone, setListDone] = useState([])
+export default class App extends Component {
+	state = {
+		todoList: [],
+		listDone: []
+	}
 
 	/**
 	 * Add item to the List
 	 */
-	function addTodoList() {
+	addTodoList = () => {
 		const newTodoItem = {
 			id: nanoid(),
 			priority: "",
@@ -18,74 +20,75 @@ function App() {
 			itemName: "",
 			isDone: false
 		}
-		setToDoList(prevList => (
-			[...prevList, newTodoItem]
-		))
+		this.setState(prevState => ({ todoList: [...prevState.todoList, newTodoItem] }))
 	}
 
 	/**
 	 * Move item from todo list to done list
 	 */
-	function markAsDone(event, itemId) {
+	markAsDone = (event, itemId) => {
 		event.stopPropagation()
-		const itemDone = todoList.find(item => item.id === itemId)
-		setToDoList(prevList => prevList.filter(item => item.id !== itemId))
-		setListDone(prevDoneList => [...prevDoneList, { ...itemDone, isDone: true }])
+		const itemDone = this.state.todoList.find(item => item.id === itemId)
+		this.setState(prevState => ({todoList: prevState.todoList.filter(item => item.id !== itemId)}))
+		this.setState(prevState => ({listDone: [...prevState.listDone, { ...itemDone, isDone: true }]}))
 	}
 
 	/**
 	 * Delete Item
 	 */
-	function deleteItem(event, itemId) {
+	deleteItem = (event, itemId) => {
 		event.stopPropagation()
-		setToDoList(prevList => prevList.filter(item => item.id !== itemId))
+		this.setState(prevState => ({todoList: prevState.todoList.filter(item => item.id !== itemId)}))
 	}
 
 	/**
 	 * Manage value change in Todo Item
 	 */
-	function handleOnChange(event, itemId) {
+	handleOnChange = (event, itemId) => {
 		event.stopPropagation()
-		const {name, value} = event.target;
-		setToDoList(prevList => prevList.map(item => {
-			return item.id === itemId ? { ...item, [name]: [value] } : item
+		const { name, value } = event.target;
+		this.setState(prevState => ({
+			todoList: prevState.todoList.map(item => {
+				return item.id === itemId ? { ...item, [name]: [value] } : item
+			})
 		}))
 	}
 
-	return (
-		<main className="main-container">
-			<div className="todo-card">
-				<div className="card-header">
-					<img src="/todo.svg" className="todo-icon" alt="To Do Icon" />
-					<h2 className="card-title">To-Do</h2>
-					<button className="add-btn" onClick={addTodoList}>
-						<img src="/add-icon.svg" className="add-icon" alt="Add Button" />
-					</button>
+	render() {
+		return (
+			<main className="main-container">
+				<div className="todo-card">
+					<div className="card-header">
+						<img src="/todo.svg" className="todo-icon" alt="To Do Icon" />
+						<h2 className="card-title">To-Do</h2>
+						<button className="add-btn" onClick={this.addTodoList}>
+							<img src="/add-icon.svg" className="add-icon" alt="Add Button" />
+						</button>
+					</div>
+					{
+						this.state.todoList.length > 0 ?
+							<TodoList
+								todoList={this.state.todoList}
+								markAsDone={this.markAsDone}
+								handleOnChange={this.handleOnChange}
+								deleteItem={this.deleteItem}
+							/> :
+							<img src="/empty.svg" className="empty-img" alt="Empty Image" />
+					}
 				</div>
-				{
-					todoList.length > 0 ?
-					<TodoList
-						todoList={todoList}
-						markAsDone={markAsDone}
-						handleOnChange={handleOnChange} 
-						deleteItem={deleteItem}
-					/> :
-					<img src="/empty.svg" className="empty-img" alt="Empty Image" />
-				}
-			</div>
-			<div className="todo-card">
-				<div className="card-header">
-					<img src="/todo-done.svg" className="todo-icon" alt="To Do Done Icon" />
-					<h2 className="card-title">Done</h2>
+				<div className="todo-card">
+					<div className="card-header">
+						<img src="/todo-done.svg" className="todo-icon" alt="To Do Done Icon" />
+						<h2 className="card-title">Done</h2>
+					</div>
+					{
+						this.state.listDone.length > 0 ?
+							<TodoList todoList={this.state.listDone} /> :
+							<img src="/empty.svg" className="empty-img" alt="Empty Image" />
+					}
 				</div>
-				{
-					listDone.length > 0 ?
-					<TodoList todoList={listDone} /> :
-					<img src="/empty.svg" className="empty-img" alt="Empty Image" />
-				}
-			</div>
-		</main>
-	)
-}
+			</main>
+		)
+	}
 
-export default App
+}
